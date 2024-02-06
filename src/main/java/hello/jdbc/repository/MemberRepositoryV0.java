4df7d597ae1333be5abc049212b3d689a18204fd
yunhye-choi <yunhye_choi@tmax.co.kr>
2024-02-06 21:18:51 +0900

    [feat] JDBC 개발 - 조회

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManager 사용
@@ -31,7 +32,60 @@ public class MemberRepositoryV0 {
             throw e;
         }
     }
+
+    public Member finaById(String memberId) throws SQLException {
+        String sql = """
+                SELECT * FROM MEMBER
+                WHERE MEMBER_ID = ?
+                """;
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return new Member(rs.getString("member_id"), rs.getInt("money"));
+            } else {
+                throw new NoSuchElementException("member not found. memberId = " + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(conn, pstmt, rs);
+        }
+    }
+
     private static Connection getConnection() {
         return DBConnectionUtil.getConnection();
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
     }
 }
