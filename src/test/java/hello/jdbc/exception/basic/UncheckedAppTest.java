8db0fbcc2be276aa4e166655df5bb4eb5d251a75
yunhye-choi <yunhye_choi@tmax.co.kr>
2024-04-23 19:54:56 +0900

    [feat] Caused by 찍기 - 예외를 던질 때는 반드시 어떤 예외를 catch 했는 지 파라미터로 넘겨주자!

--- src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
+++ src/test/java/hello/jdbc/exception/basic/UncheckedAppTest.java
@@ -1,16 +1,28 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+@Slf4j
 public class UncheckedAppTest {
     @Test
     void unchecked() {
         Controller controller = new Controller();
         Assertions.assertThatThrownBy(controller::request)
                 .isInstanceOf(RuntimeSQLException.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            log.info(e.getMessage(), e);
+        }
     }
 
     static class Controller {
@@ -55,6 +67,8 @@ public class UncheckedAppTest {
             super(cause);
         }
 
+        public RuntimeSQLException() {
+        }
     }
 
     static class RuntimeConnectException extends RuntimeException {
